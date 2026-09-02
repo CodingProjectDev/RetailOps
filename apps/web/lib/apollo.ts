3adb478b9e45ceb94cba35935ed5c85ac5693b0a
CodingProjectDev <yteasycode@gmail.com>
2026-09-02 09:48:40 -0500

    Add same-origin GraphQL proxy

--- apps/web/lib/apollo.ts
+++ apps/web/lib/apollo.ts
@@ -2,8 +2,8 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
 export const apolloClient = new ApolloClient({
   link: new HttpLink({
-    uri: process.env.NEXT_PUBLIC_GRAPHQL_URL ?? "http://localhost:4000/graphql",
-    credentials: "include"
+    uri: "/api/graphql",
+    credentials: "include",
   }),
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
